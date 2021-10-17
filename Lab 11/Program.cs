a7d1c58d974bc5bdc6757c832725e59ee0b8036b
AndreyMisyurckeev <91983861+AndreyMisyurckeev@users.noreply.github.com>
2021-10-17 20:42:56 +0300

    Добавьте файлы проекта.

---
+++ Lab 11/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение коэффициента k ");
+            double k = Convert.ToDouble(Console.Read());
+            Console.WriteLine("Введите значение коэффициента b ");
+            double b = Convert.ToDouble(Console.Read());
+            double x=0;
+            Equation equation = new Equation(b, x);
+            equation.Root();
+            Console.ReadKey();
+        }
+    }
+    public class Equation
+    {
+        double K;
+        double B;
+        double X;
+        public Equation(double k, double b, double x)
+        {
+            B = b;
+            X = x;
+        }
+        public void Root()
+        {
+            if (K<0)
+            {
+                X = B;
+                Console.WriteLine($"Значение x = {X}");
+            }
+            else if (K==0)
+            {
+                B = 0;
+                Console.WriteLine($"Значение x = {B}");
+            }
+            else
+            {
+                X = (-B) / K;
+                Console.WriteLine($"Значение x = {X}");
+            }
+        }
+    }
+    }
---
+++ Struct Equation for Lab 11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Equation_for_Lab_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation equation = new Equation();
+            Console.Write("Введите значение коэффициента k ");
+            equation.K = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение коэффициента b ");
+            equation.B = Convert.ToDouble(Console.ReadLine());
+            equation.Root();
+            Console.ReadKey();
+        }
+        struct Equation
+        {
+            public double K;
+            public double B;
+            public double X;
+            public void Root()
+            {
+                if ((K<0)||(K>0))
+                {
+                    X = (-B) / K;
+                    Console.WriteLine($"Значение x = {X:F}");
+                }
+                else 
+                {
+                    Console.WriteLine("Уравнение не имеет решения");
+                }
+            }
+        }
+    }
+}
